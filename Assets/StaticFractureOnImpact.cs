00c438db69f6e033061c522fa35cad6842ccf4a8
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-10-14 22:18:00 -0400

    Modified scene with falling projectile, added collision listener.

---
+++ Assets/StaticFractureOnImpact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticFractureOnImpact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag=="Projectile"){
+			Debug.Log("Entered collision with Projectile");
+		}
+
+	}
+}
