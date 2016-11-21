37ab633b7e6d3ab271ebd69fc8f8d6d32345a57b
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-11-21 18:28:24 -0500

    Priority queue operations complete (insert and delete)

--- resource-development/voronoi-fracture-console-app/voronoi-fracture-console-app/Program.cs
+++ resource-development/voronoi-fracture-console-app/voronoi-fracture-console-app/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
-namespace VoronoiFracture{
+namespace FortuneAlgorithm{
 	
 	class MainClass{
 		
@@ -11,15 +12,16 @@ namespace VoronoiFracture{
 			float [,] input=new float[,]{{23,43},{54,14},{34,12},{12,53},{12,56},{88,54}};
 
 			//make the event queue
-			EventQueue queue=new EventQueue();
+			PriorityQueue queue=new PriorityQueue();
 			for(int i=0;i<input.Length/2;i++){
 				queue.Push(new Event(input[i,0],input[i,1]));
 			}
-
 			while(!queue.IsEmpty()){
-				Event thisEvemt=queue.Pop();
-				if (thisEvemt!=null) {
-					thisEvemt.Handle ();
+				Event thisEvent=queue.Pop();
+				if (thisEvent!=null) {
+					thisEvent.Handle ();
+				}else{
+					break;
 				}
 			}
 		}
@@ -35,7 +37,11 @@ namespace VoronoiFracture{
 		}
 
 		virtual public void Handle(){
-				
+			Console.WriteLine(this);
+		}
+
+		public override string ToString (){
+			return "("+x+","+y+")";
 		}
 	}
 
@@ -46,26 +52,125 @@ namespace VoronoiFracture{
 			
 		}
 		override public void Handle(){
-			
+			Console.WriteLine(this);
 		}
 	}
 
-	class EventQueue{
+	class PriorityQueue{
+
+		ArrayList heap=new ArrayList();
 
 		public Event Top(){
-			return  null;
+			if(heap.Count>0){
+				return  ((Event)heap[0]);
+			}else{
+				return null;
+			}
+
 		}
 
 		public Event Pop(){
-			return null;
+			if(heap.Count==0){
+				return null;
+			}
+
+			Event lastTop=(Event)heap[0];
+
+			if(heap.Count>1){
+				object lastEvent=heap[heap.Count-1];
+				heap.RemoveAt(heap.Count-1);
+				heap[0]=lastEvent;
+				BubbleDown(0);
+			}else{
+				heap.RemoveAt(heap.Count-1);
+			}
+			return lastTop;
 		}
 
 		public void Push(Event eventNode){
-			Console.WriteLine("Inserting event "+eventNode.x+" "+eventNode.y);
+			heap.Add(eventNode);
+			BubbleUp(heap.Count-1);
+			Console.WriteLine("Inserted event "+eventNode.x+" "+eventNode.y);
 		}
 
 		public bool IsEmpty(){
 			return false;
+		}
+
+		private int Parent(int index){
+			return (index-1)/2;
+		}
+
+		private int Left(int index){
+			return index*2+1;
+		}
+
+		private int Right(int index){
+			return index*2+2;
+		}
+
+		private bool Compare(object o1,object o2){
+			return ((Event)o1).y>((Event)o2).y;
+		}
+
+		private void BubbleUp(int index){
+			while(index>0 && Compare(heap[index],heap[Parent(index)])){
+				int parentIndex=Parent(index);
+
+				//swap this position with its parent
+				object t=heap[index];
+				heap[index]=heap[parentIndex];
+				heap[parentIndex]=t;
+				index=parentIndex;
+			}
+		}
+
+		private void BubbleDown(int index){
+			int swapIndex=SmallestBetweenRootAndChildren(index);
+			while(swapIndex!=index){
+
+				object t=heap[index];
+				heap[index]=heap[swapIndex];
+				heap[swapIndex]=t;
+
+				index=swapIndex;
+				swapIndex=SmallestBetweenRootAndChildren(index);
+			}
+		}
+
+		private int SmallestBetweenRootAndChildren(int index){
+
+			int left=Left(index);
+			int right=Right(index);
+
+			//no child therefore parent is the smallest
+			if(left>=heap.Count){
+				return index;
+			}
+
+			//considering only parent and left
+			if(right>=heap.Count){
+				if(Compare(heap[index],heap[left])){
+					return index;
+				}else{
+					return left;
+				}
+			}
+
+			//considering all 3
+			if(Compare(heap[left],heap[right])){
+				if(Compare(heap[index],heap[left])){
+					return index;
+				}else{
+					return left;
+				}
+			}else{
+				if(Compare(heap[index],heap[right])){
+					return index;
+				}else{
+					return right;
+				}
+			}
 		}
 	}
 
