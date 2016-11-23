6b9a5cfd81b953475e59ab93cefac54cef528461
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-11-23 13:44:46 -0500

    Computed breakpoint of an internal node in the beach line

--- resource-development/voronoi-fracture-console-app/voronoi-fracture-console-app/Program.cs
+++ resource-development/voronoi-fracture-console-app/voronoi-fracture-console-app/Program.cs
@@ -13,13 +13,19 @@ namespace FortuneAlgorithm{
 
 			//make the event queue
 			PriorityQueue queue=new PriorityQueue();
+			BeachLine beachline=new BeachLine();
+			DoublyConnectedEdgeList dcel=new DoublyConnectedEdgeList();
 			for(int i=0;i<input.Length/2;i++){
 				queue.Push(new SiteEvent(input[i,0],input[i,1]));
 			}
+
+			InternalNode node=new InternalNode(new SiteEvent(4.83f,2.558f),new SiteEvent(3.16f,2.543f),null);
+			InternalNode.BreakPoint b=node.ComputeBreakpointAt(1f);
+			Console.WriteLine("breakpoint is "+b);
 			while(!queue.IsEmpty()){
 				Event thisEvent=queue.Pop();
 				if (thisEvent!=null) {
-					thisEvent.Handle ();
+					thisEvent.Handle (queue,beachline,dcel);
 				}else{
 					break;
 				}
@@ -36,7 +42,7 @@ namespace FortuneAlgorithm{
 			this.y=y;
 		}
 
-		abstract public void Handle();
+		abstract public void Handle(PriorityQueue queue,BeachLine beachline,DoublyConnectedEdgeList dcel);
 
 		public override string ToString (){
 			return "("+x+","+y+")";
@@ -49,8 +55,15 @@ namespace FortuneAlgorithm{
 			
 		}
 
-		override public void Handle(){
+		override public void Handle(PriorityQueue queue,BeachLine beachline,DoublyConnectedEdgeList dcel){
 			Console.WriteLine(this);
+
+			Parabola above=beachline.GetParabolaFor(x,y);
+			if(above==null){//this means that the beach line tree is empty
+				beachline.InsertRootParabola(this);	
+			}else{
+				beachline.InsertAndSplit(this,above);
+			}
 		}
 	}
 
@@ -60,7 +73,7 @@ namespace FortuneAlgorithm{
 		public CircleEvent(float x,float y):base(x,y){
 			
 		}
-		override public void Handle(){
+		override public void Handle(PriorityQueue queue,BeachLine beachline,DoublyConnectedEdgeList dcel){
 			Console.WriteLine(this);
 		}
 	}
@@ -185,38 +198,129 @@ namespace FortuneAlgorithm{
 
 	abstract class Node{
 		Node parent;
+
+		public Node(Node p){
+			parent=p;
+		}
+
 		abstract public bool IsLeaf();
+		/**
+		 * Traverses to the leaf node for the given x
+		 */
+		abstract public Node Traverse(float x,float y);
 	}
 
 	class Parabola:Node{
 		SiteEvent siteEvent;
 		CircleEvent circleEvent;
+
+		public Parabola(SiteEvent siteEvent,Node parent):base(parent){
+			this.siteEvent=siteEvent;
+		}
+
 		public override bool IsLeaf (){
 			return true;
 		}
+
+		public override Node Traverse (float x, float y){
+			return this;
+		}
 	}
 
-	class Internal:Node{
-		SiteEvent site1;
-		SiteEvent site2;
+	class InternalNode:Node{
+		public SiteEvent site1;
+		public SiteEvent site2;
 		Edge edge;
 		Node left;
 		Node right;
 
-		public override bool IsLeaf (){
+		public InternalNode(SiteEvent site1,SiteEvent site2,Node parent):base(parent){
+			this.site1=site1;
+			this.site2=site2;
+		}
+
+		public override bool IsLeaf (){			
 			return false;
+		}
+
+		public override Node Traverse (float x, float y){
+			BreakPoint p=ComputeBreakpointAt(y);
+			if(x>p.x){
+				return right;
+			}else{
+				return left;
+			}
+		}
+
+		public BreakPoint ComputeBreakpointAt(float y){
+
+			//breakpoint is retrived from the center of the circle touching the two sites and being tangent to the sweep line
+
+			//by substituting site1 and site 2 in the equation of the circle and substituting the y value of the sweepline 
+			//we can get the x value of the point at which the circle touches the sweep line or in otherwords the x of the center
+			float x=((site2.x*site2.x)+(site2.y*site2.y)-(site1.x*site1.x)-(site1.y*site1.y)+2*(site1.y)*y-2*(site2.y)*y)/(2*(site2.x-site1.x));
+
+			//now we use the x value in the equation of the perpendicular bisector between the two sites to get the y of the center
+			SiteEvent site=site1;
+			if(site1.x==x){
+				//to prevent divide by zero error while calculating slope
+				site=site2;//assumingly the perpendicular bisector will never be a vertical line with infinite slope
+			}
+			float mx=(site.x+x)/2;
+			float my=(site.y+y)/2;
+			float slope=(site.y-y)/(site.x-x); 
+			float inverseSlope=-1/slope;
+			float c=my-inverseSlope*mx;
+
+			//perpendicular bisector of a chord will always pass through the center of a circle
+			float centerY=inverseSlope*x+c;
+			return new BreakPoint(x,centerY);
+		}
+
+		public class BreakPoint{
+			public float x;
+			public float y;
+
+			public BreakPoint(float x,float y){
+				this.x=x;
+				this.y=y;
+			}
+
+			public override string ToString (){
+				return "("+x+","+y+")";
+			}
 		}
 	}
 
 	class BeachLine{
-		Parabola GetParabolaByX(float x){
+
+		Node root;
+
+		public Parabola GetParabolaFor(float x,float y){
+			if(root==null){
+				return null;
+			}
+			Node node=root;
+			do{
+				node=node.Traverse(x,y);
+			}while(!node.IsLeaf());
+
 			return null;
+		}
+
+		public void InsertRootParabola(SiteEvent siteEvent){
+			root=new Parabola(siteEvent,null);
+		}
+
+		public void InsertAndSplit(SiteEvent siteEvent,Parabola above){
+			
 		}
 	}
 
 	class Vertex{
-		float x;
-		float y;
+		public float x;
+		public float y;
+
 	}
 
 	class Face{
@@ -232,5 +336,9 @@ namespace FortuneAlgorithm{
 		Face face;
 	}
 
-
+	class DoublyConnectedEdgeList{
+		public List<Vertex> vertexList=new List<Vertex>();
+		public List<Face> faceList=new List<Face>();
+		public List<Edge> edgeList=new List<Edge>();
+	}
 }
